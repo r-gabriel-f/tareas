7feacc9a53a824fcd5c83c2d1e4256402941c2c0
robert <r.gabriel.f@outlook.com>
2023-10-04 11:54:53 -0400

    se creo login

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+import { Login } from "./components/Login/Login";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <Login/>
+  );
 }
 
 export default App;
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,7 +1,55 @@
-import React from 'react'
+import React from "react";
 
 export const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <section className="grid grid-cols-2 gap-4 h-screen border">
+      <div className="flex items-center justify-center">
+        <img
+          src="https://tecdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+          className="w-full"
+          alt="Sample image"
+        />
+      </div>
+      <div className="flex items-center justify-center ">
+        <div className="border-2 border-black rounded-md p-4 w-full mx-10">
+          <h1 className="flex justify-center text-2xl font-bold mb-4 ">INICIAR SESIÓN</h1>
+          <form>
+            <div className="mb-4">
+              <label for="username" className="block text-gray-700 font-semibold">
+                Nombre de usuario
+              </label>
+              <input
+                type="text"
+                id="username"
+                name="username"
+                className="border border-gray-400 p-2 rounded w-full"
+                placeholder="Ingrese su nombre de usuario"
+              />
+            </div>
+            <div className="mb-4">
+              <label for="password" className="block text-gray-700 font-semibold">
+                Contraseña
+              </label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                className="border border-gray-400 p-2 rounded w-full"
+                placeholder="Ingrese su contraseña"
+              />
+            </div>
+            <div className="flex justify-center">
+            <button
+              type="submit"
+              className="bg-blue-500 text-white font-semibold py-2 px-4 rounded hover:bg-blue-600"
+            >
+              Iniciar Sesión
+            </button>
+            </div>
+            
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+};
